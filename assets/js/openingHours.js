75e9f268453aaec3e8858f8110a39b106320d422
rachel <49092422+rachellee2@users.noreply.github.com>
2022-10-03 22:45:51 +1300

    sat time fixed

--- assets/js/openingHours.js
+++ assets/js/openingHours.js
@@ -1,7 +1,7 @@
   const gettingHours = new Date().getHours()
   const gettingDays = new Date().getDay()
   const weekday = ((gettingDays != 6 && gettingDays != 0) && gettingHours >= 8 && gettingHours < 17);
-  const saturday = (gettingDays == 6 && gettingHours >= 9 && gettingHours < 16);
+  const saturday = (gettingDays == 6 && gettingHours >= 9 && gettingHours < 15);
   const openClosed = (weekday || saturday) ? 'OPEN' : 'CLOSED';
 
   document.querySelector('.closeOrOpened').innerHTML = openClosed;
