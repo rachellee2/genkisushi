d726129bcaee0da5e687f745b6a4c55b01cd9bd0
rachel <49092422+rachellee2@users.noreply.github.com>
2022-10-17 21:01:08 +1300

    test get current date

--- assets/js/openingHours.js
+++ assets/js/openingHours.js
@@ -1,8 +1,21 @@
   const gettingHours = new Date().getHours()
   const gettingDays = new Date().getDay()
+  // const current
+
+  const holiday = new Map([
+    // Test 
+    ["CLOSED :( t", "17102022"],
+
+    ["Labour Day", "24102022"],
+    ["Christmas Day", "25122022"],
+    ["Boxing Day", "26122022"]
+  ]);
+
+  const currentDate = new Date().getDate() + new Date().getMonth() + new Date().getFullYear();
   const weekday = ((gettingDays != 6 && gettingDays != 0) && gettingHours >= 8 && gettingHours < 17);
   const saturday = (gettingDays == 6 && gettingHours >= 9 && gettingHours < 15);
-  const openClosed = (weekday || saturday) ? 'we are currently OPEN' : 'we are currently CLOSED';
+  const openClosed = (weekday || saturday) ? 'we are currently OPEN' : 'we are currently CLOSED' + currentDate;
+
 
   document.querySelector('.closeOrOpened').innerHTML = openClosed;
   
@@ -12,7 +25,6 @@
   else{
     document.querySelector('.hours').style.backgroundColor = '#643a39';
   }
-
 
   if(gettingDays == 0){
     document.querySelector('.whatDay').innerHTML = "Sunday | Closed";
